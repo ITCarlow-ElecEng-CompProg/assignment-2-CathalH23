9f673f68100c72c9f6370619239a6b8d5cd32414
CathalH23 <34032430+CathalH23@users.noreply.github.com>
2017-11-27 14:59:31 +0000

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,36 @@
+/** Factorial Calculator
+ *  Cathal Hogan
+ *  C00184521
+ * 25-09-2017
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{   // Variable Declaration
+    int num, i; int fact = 1;
+    //Cout displays prompts user to enter a value
+    cout << "Enter a number for n:"<<endl;
+    cin >> num;
+
+    //While Loop ensures number is is positive
+    while (num<0)
+    {
+        cout<< "Enter a number >0"<<endl;
+        cin>> num;
+    }
+
+    //For loop
+    for(i=num;i>0;i--)
+    {
+        fact = fact* i;
+    }
+
+    //Cout Displays answer on screen
+    cout<< "Factorial is : "<< fact;
+
+    return 0;
+}
